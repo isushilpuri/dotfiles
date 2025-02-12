ab7dd5b2419b9b6377722f82306b18ee63460c8b
Sushil Kumar <isushilpuri@gmail.com>
2025-02-12 11:41:07 +0530

    add restructured dotfiles

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,16 +1,17 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    css = { "prettier" },
     python = { "black" },
+    -- css = { "prettier" },
     -- html = { "prettier" },
   },
 
   format_on_save = {
     -- These options will be passed to conform.format()
-    lsp_fallback = true,
     timeout_ms = 500,
+    lsp_fallback = true,
   },
 }
 
-require("conform").setup(options)
+return options
+
